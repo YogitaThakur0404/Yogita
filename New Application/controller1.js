7dfbebf9dcedc4681edea6472e6f3398a27707e1
Yogita <Yogita@Yogita.local>
2018-08-14 13:59:13 +0530

    new App in angular

---
+++ New Application/controller1.js
@@ -0,0 +1,5 @@
+myapp.controller("testcontroller1",['$scope',function($scope){
+$scope.name="Yogita";
+$scope.emailId="yogita.thakur@qliktag.com";
+
+}])
---
+++ New Application/controller2.js
@@ -0,0 +1,5 @@
+myapp.controller("testcontroller2",['$scope',function($scope){
+$scope.name="Rose";
+$scope.color="Red";
+
+}])
