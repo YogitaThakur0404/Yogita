a01c5b73fed5f7c978ce26c5542b7cfbf5e63d2c
Yogita <Yogita@Yogita.local>
2018-08-20 16:45:26 +0530

    calculator app with routing para

---
+++ calculatorwithrouting/module.js
@@ -0,0 +1,25 @@
+
+        var myApp = angular.module("myApp", ["ngRoute"]);
+       
+
+        myApp.config('$routeProvider',function($routeProvider) {
+            $routeProvider
+            .when("/", {
+                templateUrl : "main.html"
+            })
+            .when("/calculator", {
+                templateUrl : "calculator.html",
+                controller:"calCtrl"
+            })
+            
+            when('/calculator/:number1/:number2', {
+                templateUrl: 'calculator.html',
+                controller: 'calCtrl'
+            })
+            .when('/calculator/:number1', {
+                templateUrl: 'calculator.html',
+                controller: 'calCtrl'
+            })
+            
+        });
+        
