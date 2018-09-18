2c6c931bbceae990567db0e7bd925a546d5b559e
Yogita <Yogita@Yogita.local>
2018-09-18 20:01:51 +0530

    cmp app

--- companyapp(18:sept)/server.js
+++ companyapp(18:sept)/server.js
@@ -13,6 +13,8 @@ var async = require('async');
 var companys = require("./company.model")
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/companydb"); //company database
+
+
 // 1.This responds a GET request for the /show page for all companys.
 app.get("/show", function(req, res) {
     console.log("get all users");
@@ -27,6 +29,8 @@ app.get("/show", function(req, res) {
 })
 
 // 2. This responds a GET request for the status
+// Get all comapnies having `status:activated` from DB
+
 app.get("/all", function(req, res) {
 
     async.series([
@@ -44,60 +48,55 @@ app.get("/all", function(req, res) {
 
                 }
             })
-
         },
-
     ], function(error, data) {
-
         if (error) {
             res.send(error);
-
         } else {
             console.log("nm=" + data.companyName);
             res.send(data);
         }
-
     });
-
 })
 
 // 3 This responds a GET request for the companyName
-app.get("/all/:companyName", function(req, res) {
+// GET by companyName,
+// 1. companyName - Consider as req params in the endpoint
+// 2. Get company detail having `status:activated` and `companyName` provided in req params
+// 3. If company not found in DB display error-Company Not found
 
+app.get("/all/:companyName", function(req, res) {
         async.series([
             function(callback) {
-
-                companys.findOne({ "companyName": req.params.companyName }, function(err, data) {
+                //   companys.findOne({ "companyName": req.params.companyName }, function(err, data) {
+                companys.findOne({ $and: [{ "status": "activated" }, { companyName: req.params.companyName }] }, function(error, data) {
                     if (!data) {
-                        // res.send("error : user not exist ");
+
                         callback("company not found");
                     } else {
                         console.log("company=" + data.companyName);
-                        // res.json(data);
                         callback(null, data);
-
                     }
                 })
-
             },
-
         ], function(error, data) {
-
             if (error) {
                 res.send(error);
-
             } else {
                 res.send(data);
             }
-
         });
-
     })
     //4 get
+    /*
+    GET by companyName and state
+1. companyName - Consider as req params
+2. state - Consider as req query
+2. Get company detail having `status:activated` and `companyName` provided in req params and `state` provided in req query
+3. If company not found in DB display error-Company Not found
+    */
 app.get("/company/:companyName", function(req, res) {
         console.log("get single user by query");
-
-
         async.series([
             function(callback) {
                 companys.find({ $and: [{ "status": "activated" }, { companyName: req.params.companyName }, { state: req.query.state }] }, function(error, data) {
@@ -112,7 +111,6 @@ app.get("/company/:companyName", function(req, res) {
                     }
                 })
             },
-
         ], function(error, Data) {
             if (error) {
                 res.send(error);
@@ -122,12 +120,19 @@ app.get("/company/:companyName", function(req, res) {
             }
         })
     })
-    // 4 This responds a POST request for the /users page by email . 
+    /*
+    POST -
+1. Create new company
+2. Check company already exist with `companyName` and `status:activated`
+3. If company exist display error- company already exist
+4. Else create new company
+    */
+    // 4 This responds a POST request 
 app.post("/company", function(req, res) {
         var cmp = req.body;
         async.series([
             function(callback) {
-                companys.findOne({ $and: [{ companyName: cmp.companyName }] }, function(error, existingcmp) {
+                companys.find({ $and: [{ companyName: cmp.companyName }, { status: "activated" }] }, function(error, existingcmp) {
                     if (existingcmp == null) {
                         companys.create(req.body, function(error, data) {
                             if (error) {
@@ -151,6 +156,50 @@ app.post("/company", function(req, res) {
         });
     })
     //5 This responds a PUT request 
+    /*
+PUT - by companyName
+1. update company : Consider companyName as req prams
+2. Check company exist or not with `companyName` and `status:activated`
+3. If company exist - update company with new data
+4. Else throw error- company does not exist
+    */
+
+app.put("/company/:companyName", function(req, res) {
+
+    async.series([
+        function(callback) {
+            // companys.findOne({ companyName: req.params.companyName }, function(err, existinguser) {
+            companys.find({ $and: [{ companyName: req.params.companyName }, { status: "activated" }] }, function(error, existingcmp) {
+                if (existingcmp != null) {
+                    companys.updateOne({ companyName: req.params.companyName }, { $set: { address: req.body.address, country: req.body.country, state: req.body.state, city: req.body.city, status: req.body.status } }, function(err, data) { //updating by updateOne()
+
+                        if (!data) {
+                            console.log("not updated");
+                            callback("not updated");
+                        } else {
+                            callback(null, data);
+                        }
+                    })
+                } else {
+                    callback(" error- company does not exist");
+                }
+            })
+        },
+    ], function(error, data) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.send(data);
+        }
+    });
+})
+
+//    PUT - by state
+//    1. Consider state as req query
+//    2. update company with `status: deactivated` having `status:activated` and `state` from req query
+//    3. If companies exist with above queries - update company with new data
+//    4. Else throw error- company does not exist
+
 app.put("/company", function(req, res) {
         var state = req.query.state;
         console.log("state=" + state);
@@ -180,6 +229,14 @@ app.put("/company", function(req, res) {
             }
         });
     })
+    /*
+DELETE by comapnyName
+1. delete company: Consider companyName as req prams
+2. Check company exist or not with `companyName` and `status:activated`
+3. If company exist - update status as deleted
+4. Else throw error- company does not exist
+use `mongoose` module for DB related queries
+    */
     // 6 This responds a DELETE request for the /users page by email . 
 app.delete("/company/:companyName", function(req, res) {
     var name = req.params.companyName;
@@ -209,7 +266,7 @@ app.delete("/company/:companyName", function(req, res) {
         }
     });
 })
-var server = app.listen(8028, function() {
+var server = app.listen(8058, function() {
 
     var host = server.address().address
     var port = server.address().port
